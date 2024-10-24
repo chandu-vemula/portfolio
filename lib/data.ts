1b10537d4439870baaba133624a110a910053c4e
chandu-vemula <chanduvemula1996@gmail.com>
2024-10-24 13:08:04 +0530

    Add and style the Header component

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import EcommImg from "@/public/EcommImg.png";
+import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
@@ -66,7 +66,7 @@ export const projectsData = [
     description:
       "I worked as a developer on this startup project for 1 year and 8 months. Focused on unique shopping experience.",
     tags: ["React", "BootStrap", "Javascript", "HTML", "CSS"],
-    imageUrl: EcommImg,
+    imageUrl: corpcommentImg,
   },
   {
     title: "Uber for Business Dashboard",
