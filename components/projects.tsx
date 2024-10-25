9b3c422410527ab04ea02f7d6713c59733f137d7
chandu-vemula <chanduvemula1996@gmail.com>
2024-10-25 12:19:07 +0530

    complete skills section with animations

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data';
 import Project from './project';
@@ -12,7 +12,7 @@ export default function Projects() {
  const { ref } = useSectionInView("Projects", 0.5);
 
   return (
-    <section ref={ref} id="projects" className='scroll-mt-28'>
+    <section ref={ref} id="projects" className='scroll-mt-28 mb-28'>
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
