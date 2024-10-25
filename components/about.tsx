d2756f54ed0554ee2295f0a11dc165957b4f3032
chandu-vemula <chanduvemula1996@gmail.com>
2024-10-25 07:23:50 +0530

    use clsx to conditionally create classNames for scrolling to the sections when clicked on header

--- components/about.tsx
+++ components/about.tsx
@@ -6,10 +6,11 @@ import { motion } from 'framer-motion'
 
 export default function About() {
   return (
-    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
      initial = {{ opacity: 0, y: 100}}
      animate = {{ opacity: 1, y: 0}}
      transition = {{delay: 0.175}}
+     id="about"
     >
       <SectionHeading>About me</SectionHeading>
       <p>
@@ -28,6 +29,7 @@ export default function About() {
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
+      <br/>
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy reading,
         watching movies, and riding my motorcycle. I also enjoy{" "}
